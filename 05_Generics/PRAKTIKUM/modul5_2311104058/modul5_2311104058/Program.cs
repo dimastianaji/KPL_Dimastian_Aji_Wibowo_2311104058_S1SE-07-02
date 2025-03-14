28a4eff3d0d0204d8facf9317a5aa6eae35f36e3
Dimastian Aji Wibowo <147970502+dimastianaji@users.noreply.github.com>
2025-03-14 19:11:41 +0700

    Upload project praktikum

---
+++ 05_Generics/PRAKTIKUM/modul5_2311104058/modul5_2311104058/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        List<int> arr = new List<int> { 0, 1, 2, 3, 4 }; // Gunakan List<int> sebagai pengganti array
+        List<int> list = new List<int>();
+        for (int x = 5; x < 10; x++)
+        {
+            list.Add(x);
+        }
+        ProcessItems<int>(arr);
+        ProcessItems<int>(list);
+    }
+
+    static void ProcessItems<T>(IList<T> coll)
+    {
+        // IsReadOnly returns True for array-based collections and False for List<T>.
+        Console.WriteLine("IsReadOnly returns {0} for this collection.", coll.IsReadOnly);
+
+        // Hapus elemen ke-4 jika koleksi tidak read-only
+        if (!coll.IsReadOnly)
+        {
+            coll.RemoveAt(4);
+        }
+
+        foreach (T item in coll)
+        {
+            Console.Write(item.ToString() + " ");
+        }
+        Console.WriteLine();
+    }
+}
