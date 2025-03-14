7897dee2a090cbc3a4f455d5922b75c6d8870dee
Dimastian Aji Wibowo <147970502+dimastianaji@users.noreply.github.com>
2025-03-14 13:29:01 +0700

    Add files via upload

---
+++ 05_Generics/TP/kpl_tpmodul5_2311104058/kpl_tpmodul5_2311104058/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class HaloGeneric
+{
+    // Method generic untuk menyapa user
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine($"Halo user {user}");
+    }
+}
+
+class DataGeneric<T>
+{
+    private T data;
+
+    // Konstruktor untuk menginisialisasi data
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    // Method untuk mencetak data
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {data}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        HaloGeneric halo = new HaloGeneric();
+
+        // Meminta input nama dari user
+        Console.Write("Masukkan nama Anda: ");
+        string nama = Console.ReadLine();
+
+        // Memanggil method dengan input dari user
+        halo.SapaUser(nama);
+
+        // Meminta input NIM dari user
+        Console.Write("Masukkan NIM Anda: ");
+        string nim = Console.ReadLine();
+
+        // Membuat objek DataGeneric dengan tipe string
+        DataGeneric<string> dataGeneric = new DataGeneric<string>(nim);
+
+        // Memanggil method PrintData
+        dataGeneric.PrintData();
+    }
+}
