c83fdf98320087d8dee7cb254c3f031606714f76
Dimastian Aji Wibowo <dimastian.exe@gmail.com>
2025-04-19 16:25:20 +0700

    Upload file project dan file pdf Jurnal 7

---
+++ 07_Grammar-Based_Input_Processing_Parsing/JURNAL/modul7_kelompok_1/modul7_kelompok_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using modul7_kelompok_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var data = new DataMahasiswa2311104058();
+        data.ReadJSON();
+
+        Console.WriteLine(); 
+
+        TeamMembers2311104058.ReadJSON();
+
+        Console.WriteLine();
+
+        GlossaryItem2311104058.ReadJSON();
+    }
+}
