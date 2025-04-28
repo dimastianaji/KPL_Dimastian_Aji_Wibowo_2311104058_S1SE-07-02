42d61715b5853dc242723f2833fde26680cde952
Dimastian Aji Wibowo <dimastian.exe@gmail.com>
2025-04-29 00:52:41 +0700

    menambahkan API Movies

---
+++ 09_API_Design_dan_Construction_Using_Swagger/JURNAL/modul9_2311104058/modul9_2311104058/Movie/MoviesController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieApi.Models;
+using System.Collections.Generic;
+using modul9_2311104058;
+
+namespace MovieApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private static List<Movie> movies = new List<Movie>
+        {
+            new Movie
+            {
+                Title = "The Shawshank Redemption",
+                Director = "Frank Darabont",
+                Stars = new List<string> { "Tim Robbins", "Morgan Freeman", "Bob Gunton" },
+                Description = "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency."
+            },
+            new Movie
+            {
+                Title = "The Godfather",
+                Director = "Francis Ford Coppola",
+                Stars = new List<string> { "Marlon Brando", "Al Pacino", "James Caan" },
+                Description = "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son."
+            },
+            new Movie
+            {
+                Title = "The Dark Knight",
+                Director = "Christopher Nolan",
+                Stars = new List<string> { "Christian Bale", "Heath Ledger", "Aaron Eckhart" },
+                Description = "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests."
+            }
+        };
+        // GET: api/Movies
+        [HttpGet]
+        public ActionResult<IEnumerable<Movie>> GetMovies()
+        {
+            return movies;
+        }
+        // GET: api/Movies/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Movie> GetMovie(int id)
+        {
+            if (id < 0 || id >= movies.Count)
+                return NotFound();
+            return movies[id];
+        }
+        // POST: api/Movies
+        [HttpPost]
+        public ActionResult<IEnumerable<Movie>> PostMovie(Movie movie)
+        {
+            movies.Add(movie);
+            return movies;
+        }
+        // DELETE: api/Movies/{id}
+        [HttpDelete("{id}")]
+        public ActionResult<IEnumerable<Movie>> DeleteMovie(int id)
+        {
+            if (id < 0 || id >= movies.Count)
+                return NotFound();
+
+            movies.RemoveAt(id);
+            return movies;
+        }
+    }
+}
