043d5ec439cd0f0f5882a34eae66b04f077587b5
Dimastian Aji Wibowo <dimastian.exe@gmail.com>
2025-05-08 18:30:55 +0700

    menambahkan file TP modul 10

---
+++ 10_Library_Construction/TP/tpmodul10_2311104058/AljabarConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using AljabarLibraries;
+
+namespace AljabarConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] akar = Aljabar.AkarPersamaanKuadrat(new double[] { 1, -3, -10 });
+            Console.WriteLine("Akar-akarnya: " + string.Join(", ", akar));
+
+            double[] hasilKuadrat = Aljabar.HasilKuadrat(new double[] { 2, -3 });
+            Console.WriteLine("Hasil kuadrat: " + string.Join(", ", hasilKuadrat));
+        }
+    }
+}
