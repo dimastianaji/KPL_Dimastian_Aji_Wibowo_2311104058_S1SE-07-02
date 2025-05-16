bc1dbbb8540f02ad642e6d227b505a6eca8dbef5
Dimastian Aji Wibowo <dimastian.exe@gmail.com>
2025-05-16 10:23:17 +0700

    menambahkan file project praktikum modul 13

---
+++ 13_Design_Pattern_Implementation/PRAKTIKUM/implementdp/Adapter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adapter_dp
+{
+    public interface IAmericanPlug
+    {
+        void Plugin();
+    }
+
+    public class IndonesianPlug
+    {
+        public void Sambungkan()
+        {
+            Console.WriteLine("Colokan Indonesia tersambung");
+        }
+    }
+
+    public class PlugAdaptor : IAmericanPlug
+    {
+        private readonly IndonesianPlug _plug;
+
+        public PlugAdaptor(IndonesianPlug plug)
+        {
+            _plug = plug;
+        }
+
+        public void Plugin()
+        {
+            Console.WriteLine("Menggunakan adapter colokan Indonesia ke Mamarika");
+            _plug.Sambungkan();
+        }
+    }
+}
---
+++ 13_Design_Pattern_Implementation/PRAKTIKUM/implementdp/Command.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace command_dp
+{
+    public interface ICommand
+    {
+        void Execute();
+    }
+
+    public class Television
+    {
+        public void TurnOn()
+        {
+            Console.WriteLine("Television is on");
+        }
+
+        public void TurnOff()
+        {
+            Console.WriteLine("Television is off");
+        }
+    }
+
+    public class TelevisionTurnOn : ICommand
+    {
+        private readonly Television _television;
+
+        public TelevisionTurnOn(Television television)
+        {
+            _television = television;
+        }
+
+        public void Execute()
+        {
+            _television.TurnOn();
+        }
+    }
+
+    public class TelevisionTurnOff : ICommand
+    {
+        private readonly Television _television;
+
+        public TelevisionTurnOff(Television television)
+        {
+            _television = television;
+        }
+
+        public void Execute()
+        {
+            _television.TurnOff();
+        }
+    }
+
+    public class RemoteTV
+    {
+        private ICommand? turnOn;
+        private ICommand? turnOff;
+
+        public void SetTurnOn(ICommand command)
+        {
+            turnOn = command;
+        }
+
+        public void SetTurnOff(ICommand command)
+        {
+            turnOff = command;
+        }
+
+        public void TurnOnTV()
+        {
+            turnOn?.Execute();
+        }
+
+        public void TurnOffTV()
+        {
+            turnOff?.Execute();
+        }
+    }
+}
