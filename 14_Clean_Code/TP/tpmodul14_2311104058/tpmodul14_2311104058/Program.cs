bc16d502249359b1fffa992b93b244b28484ef8f
Dimastian Aji Wibowo <dimastian.exe@gmail.com>
2025-05-17 12:14:30 +0700

    menambahkan file project TP modul 14 dan file TP - Modul 14 - 2311104058.pdf

---
+++ 14_Clean_Code/TP/tpmodul14_2311104058/tpmodul14_2311104058/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace TPModul14
+{
+    /// <summary>
+    /// Class untuk menyapa user menggunakan method generic
+    /// </summary>
+    class GreetingService
+    {
+        /// <summary>
+        /// Method generic untuk menyapa user
+        /// </summary>
+        /// <typeparam name="T">Tipe data user (contoh: string)</typeparam>
+        /// <param name="user">Data user yang akan disapa</param>
+        public void GreetUser<T>(T user)
+        {
+            Console.WriteLine($"Halo user {user}");
+        }
+    }
+
+    /// <summary>
+    /// Class untuk menyimpan dan mencetak data generic
+    /// </summary>
+    /// <typeparam name="T">Tipe data yang akan disimpan</typeparam>
+    class DataContainer<T>
+    {
+        private T data;
+
+        /// <summary>
+        /// Konstruktor untuk menginisialisasi data
+        /// </summary>
+        /// <param name="data">Data yang akan disimpan</param>
+        public DataContainer(T data)
+        {
+            this.data = data;
+        }
+
+        /// <summary>
+        /// Method untuk mencetak data
+        /// </summary>
+        public void PrintData()
+        {
+            Console.WriteLine($"Data yang tersimpan adalah: {data}");
+        }
+    }
+
+    /// <summary>
+    /// Entry point program
+    /// </summary>
+    class Program
+    {
+        static void Main()
+        {
+            GreetingService greetingService = new GreetingService();
+
+            // Meminta input nama dari user
+            Console.Write("Masukkan nama Anda: ");
+            string nama = Console.ReadLine();
+
+            // Menyapa user
+            greetingService.GreetUser(nama);
+
+            // Meminta input NIM dari user
+            Console.Write("Masukkan NIM Anda: ");
+            string nim = Console.ReadLine();
+
+            // Membuat objek DataContainer dengan tipe string
+            DataContainer<string> dataContainer = new DataContainer<string>(nim);
+
+            // Menampilkan data
+            dataContainer.PrintData();
+        }
+    }
+}
